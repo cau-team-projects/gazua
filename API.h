2bed5091bd2cbe09c4a82421b24097e9a7faa691
Byeonggon Lee <gonny952@gmail.com>
2019-11-26 22:36:34 +0900

    Modify API class

--- API.h
+++ API.h
@@ -15,11 +15,11 @@ public:
     void refresh();
     // GET https://api.korbit.co.kr/v1/ticker/detailed/all
     // GET https://api.korbit.co.kr/v1/constants
-    std::unordered_map<std::string, CoinInfo> refreshCoinInfos();
+    bool refreshCoinInfos(std::shared_ptr<std::unordered_map<std::string, CoinInfo>> coinInfos);
     // GET https://api.korbit.co.kr/v1/user/balances
-    std::unordered_map<std::string, Balance> refreshBalances();
+    bool refreshBalances(std::shared_ptr<std::unordered_map<std::string, Balance>> balances);
     // GET https://api.korbit.co.kr/v1/user/volume
-    std::unordered_map<std::string, VolumeFee> refreshVolumeFees();
+    bool refreshVolumeFees(std::shared_ptr<std::unordered_map<std::string, VolumeFee>> volumeFees);
     // POST https://api.korbit.co.kr/v1/user/orders/buy
     bool buy(const std::string& coinName, OrderType orderType, uint64_t price, AmountType amountType, uint64_t amount);
     // POST https://api.korbit.co.kr/v1/user/orders/sell
@@ -27,5 +27,5 @@ public:
     // POST https://api.korbit.co.kr/v1/user/orders/cancel
     bool cancel(const std::string& coinName, const std::vector<uint64_t>& ids);
     // GET https://api.korbit.co.kr/v1/user/orders
-    std::map<uint64_t, Order> getOrders(const std::string& coinName, const std::vector<uint64_t>& ids, uint64_t limit = 10);
+    getOrders(std::shared_ptr<std::map<uint64_t, Order>> orders, const std::string& coinName, const std::vector<uint64_t>& ids, uint64_t limit = 10);
 };
