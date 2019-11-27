33c745967b15c0735627c6de1c6dab793afaf6f6
snapperbay4453 <snapperbay4453@gmail.com>
2019-11-27 09:46:53 +0900

    Implement Order class

--- Order.h
+++ Order.h
@@ -2,12 +2,26 @@
 #define ORDER_H
 
 namespace Gazua {
-    class Wallet;
+    class Order;
 };
 
 class Gazua::Order {
-public:
-    long double available
-};
+private:
+    std::string currency_pair,		// 해당 통화쌍
+                side;				// 매수/매도 구분 (매수: bid, 매도: ask)
+    uint64_t    avg_price,			// 체결 가격의 가중 평균치
+                price;				// 주문 시에 설정한 지정가, 기본값은 0 
+    long double order_amount,		// 주문 시에 지정한 코인의 수량
+                filled_amount,		// 현재까지 체결된 코인의 수량
+    uint64_t    order_total,		// 주문 금액
+                filled_total,		// 체결 금액
+                created_at,			// 거래를 주문한 시각 Unix timestamp(milliseconds)
+                last_filled_at;		// 거래가 부분 체결된 최종 시각 Unix timestamp(milliseconds)
+    std::string status;				// 현재 주문의 상태 ('unfilled', 'partially_filled', 'filled')
+    long double fee;				// 거래 수수료
+    // id(주문의 ID 식별번호)는 map 자료형에서 key값으로 사용 예정
+public:		
 
-#endif
+}
+
+#endif정 
