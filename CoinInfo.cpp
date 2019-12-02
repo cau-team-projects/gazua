5e38a4bdaddc1d09766e84a1253995beeff65c83
snapperbay4453 <snapperbay4453@gmail.com>
2019-12-02 20:09:33 +0900

    Fix refreshCoinInfos method

--- CoinInfo.cpp
+++ CoinInfo.cpp
@@ -1,5 +1,23 @@
 #include "API.h"
 
+Gazua::CoinInfo::CoinInfo()
+{
+    timestamp = 0.0;
+    last = 0.0;
+    open = 0.0;
+    bid = 0.0;
+    ask = 0.0;
+    low = 0.0;
+    high = 0.0;
+    volume = 0.0;
+    change = 0.0;
+    changePercent = 0.0;
+    tick_size = 0.0;
+    min_price = 0.0;
+    max_price = 0.0;
+    order_min_size = 0.0;
+    order_max_size = 0.0;
+}
 
 
 Gazua::CoinInfo::CoinInfo(double _timestamp, double _last, double _open, double _bid, double _ask, double _low, double _high, double _volume, double _change, double _changePercent, double _tick_size, double _min_price, double _max_price, double _order_min_size, double _order_max_size)
--- CoinInfo.h
+++ CoinInfo.h
@@ -29,6 +29,7 @@ public:
             order_min_size, //매수/매도 수량 최소 입력값
             order_max_size; //매수/매도 수량 최대 입력값    
 
+    CoinInfo();
     CoinInfo(double _timestamp, double _last, double _open, double _bid, double _ask, double _low, double _high, double _volume, double _change, double _changePercent, double _tick_size, double _min_price, double _max_price, double _order_min_size, double _order_max_size);
 
 };
