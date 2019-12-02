0fc4143941c86a98cd58842de4d0706f15a80efa
snapperbay4453 <snapperbay4453@gmail.com>
2019-12-02 22:51:12 +0900

    Modify CoinModel.h

--- CoinModel.h
+++ CoinModel.h
@@ -23,7 +23,7 @@ public:
         ValueRole
     };
 
-    explicit CoinModel(QObject *parent = nullptr, QTimer timer);
+    explicit CoinModel(QObject *parent = nullptr);
     int rowCount(const QModelIndex& parent = QModelIndex()) const;
     int columnCount(const QModelIndex& parent = QModelIndex()) const;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
