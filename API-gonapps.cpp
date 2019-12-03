a0fd685dd8677433e082bc6ea5a5e36b7fa77b9e
Byeonggon Lee <gonny952@gmail.com>
2019-12-03 21:30:58 +0900

    Rename variables

--- API-gonapps.cpp
+++ API-gonapps.cpp
@@ -18,9 +18,9 @@ bool Gazua::API::access() {
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
     auto reply = m_qnam.post(request, query.query().toUtf8());
     connect(reply, &QNetworkReply::finished, this, [this, reply] () {
-        const auto jsonObj = QJsonDocument::fromJson(reply->readAll()).object();
-        const auto tokenType = jsonObj["token_type"].toString() == "Bearer" ? TokenType::BEARER : TokenType::UNKNOWN;
-        const auto scopeStringList = jsonObj["scope"].toString().split(',', QString::SkipEmptyParts);
+        const auto doc = QJsonDocument::fromJson(reply->readAll()).object();
+        const auto tokenType = doc["token_type"].toString() == "Bearer" ? TokenType::BEARER : TokenType::UNKNOWN;
+        const auto scopeStringList = doc["scope"].toString().split(',', QString::SkipEmptyParts);
         uint32_t scope = 0;
         for(const auto& scopeString : scopeStringList) {
             if(scopeString == "VIEW")
@@ -30,14 +30,15 @@ bool Gazua::API::access() {
             else if(scopeString == "WITHDRAWL")
                 scope = scope | Token::WITHDRAWL;
         }
-        if(jsonObj["access_token"].isUndefined()) {
+        if(doc["access_token"].isUndefined()) {
             m_token = std::nullopt;
             qDebug() << "Failed to fetch token";
         } else {
-            const auto expiration = jsonObj["expires_in"].toInt();
-            const auto accessToken = jsonObj["access_token"].toString();
-            const auto refreshToken = jsonObj["refresh_token"].toString();
+            const auto expiration = doc["expires_in"].toInt();
+            const auto accessToken = doc["access_token"].toString();
+            const auto refreshToken = doc["refresh_token"].toString();
             m_token.emplace(Token{tokenType, scope, static_cast<uint32_t>(expiration), accessToken, refreshToken});
+            qDebug() << m_token.value();
         }
         reply->close();
         // reply->deleteLater(); not sure
@@ -62,18 +63,18 @@ bool Gazua::API::refresh() {
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
     auto reply = m_qnam.post(request, query.query().toUtf8());
     connect(reply, &QNetworkReply::finished, this, [this, reply] () {
-        const auto jsonObj = QJsonDocument::fromJson(reply->readAll()).object();
-        const auto tokenType = jsonObj["token_type"].toString() == "Bearer" ? TokenType::BEARER : TokenType::UNKNOWN;
-        const auto scopeStringList = jsonObj["scope"].toString().split(',', QString::SkipEmptyParts);
+        const auto doc = QJsonDocument::fromJson(reply->readAll()).object();
+        const auto tokenType = doc["token_type"].toString() == "Bearer" ? TokenType::BEARER : TokenType::UNKNOWN;
+        const auto scopeStringList = doc["scope"].toString().split(',', QString::SkipEmptyParts);
         uint32_t scope = 0;
-        if(jsonObj["access_token"].isUndefined()) {
+        if(doc["access_token"].isUndefined()) {
             m_token = std::nullopt;
             qDebug() << "Failed to fetch token";
         } else {
-            const auto expiration = jsonObj["expires_in"].toInt();
-            const auto accessToken = jsonObj["access_token"].toString();
-            const auto refreshToken = jsonObj["refresh_token"].toString();
-            m_token = Token{tokenType, scope, static_cast<uint32_t>(expiration), accessToken, refreshToken};
+            const auto expiration = doc["expires_in"].toInt();
+            const auto accessToken = doc["access_token"].toString();
+            const auto refreshToken = doc["refresh_token"].toString();
+            m_token.emplace(Token{tokenType, scope, static_cast<uint32_t>(expiration), accessToken, refreshToken});
             qDebug() << m_token.value();
         }
         reply->close();
@@ -92,7 +93,7 @@ bool Gazua::API::refreshUserInfo(std::shared_ptr<UserInfo> userInfo) {
     request.setRawHeader(QString{"Authorization"}.toUtf8(), QString{"Bearer %1"}.arg(accessToken).toUtf8());
     auto reply = m_qnam.get(request);
     connect(reply, &QNetworkReply::finished, this, [reply, userInfo] () {
-        qDebug() << reply->readAll();
+        const auto doc = QJsonDocument::fromJson(reply->readAll());
     });
     return true;
 }
