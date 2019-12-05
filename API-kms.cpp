176b40fe54417011d460489f62b9f977a3004aa7
mskim9967 <mskim9967@gmail.com>
2019-12-05 22:09:20 +0900

    FIX don't get parameter

--- API-kms.cpp
+++ API-kms.cpp
@@ -6,9 +6,8 @@
 #include <QJsonDocument>
 
 bool Gazua::API::getOrders(std::shared_ptr<QMap<uint64_t, Order>> orders, const QString& coinName, const QVector<uint64_t>& ids, uint64_t limit){
-/*
-    QUrlQuery qUrl;
-    QString tmp("");
+
+    /*
     qUrl.addQueryItem("currency_pair", coinName);
     for(auto i = ids.begin(); i != ids.end(); i++)
         tmp.append(QString("id=%1&").arg(QString::number(*i)));
@@ -16,7 +15,6 @@ bool Gazua::API::getOrders(std::shared_ptr<QMap<uint64_t, Order>> orders, const 
     qUrl.addQueryItem("id", tmp);
     qUrl.addQueryItem("limit", QString::number(limit));
 
-    QNetworkRequest network;
     if(!this->m_token.has_value())
         return false;
     const auto accessToken = this->m_token->accessToken();
@@ -27,23 +25,39 @@ bool Gazua::API::getOrders(std::shared_ptr<QMap<uint64_t, Order>> orders, const 
 
     network.setRawHeader(QByteArray("Authorization"), header.toUtf8());
     network.setUrl(qUrl.toString());
+    */
+    const auto accessToken = this->m_token->accessToken();
+    if(!accessToken.has_value())
+        return false;
+    const auto header = QString("Bearer %1").arg(accessToken.value());
 
-    auto reply = m_qnam.get(network);
+    QNetworkRequest network;
+    network.setRawHeader(QByteArray("Authorization"), header.toUtf8());
+    network.setUrl(QUrl{"https://api.korbit.co.kr/v1/user/orders"});
+
+    QNetworkReply* reply = m_qnam.get(network);
     connect(reply, &QNetworkReply::finished, this, [reply, orders]() {
         auto jsonArr =  QJsonDocument::fromJson(reply->readAll()).array();
 
         for(auto iter = jsonArr.begin(); iter != jsonArr.end(); iter++) {
-            Order order((*iter).toObject()["currency_pair"].toString(), (*iter).toObject()["side"].toString()
-                    ,(*iter).toObject()["avg_price"].toInt(),(*iter).toObject()["price"].toInt()
-                    ,(*iter).toObject()["order_amount"].toDouble(),(*iter).toObject()["filled_amount"].toDouble()
-                    ,(*iter).toObject()["order_total"].toInt(), (*iter).toObject()["filled_total"].toInt()
-                    ,(*iter).toObject()["created_at"].toInt(), (*iter).toObject()["last_filled_at"].toInt()
-                    ,(*iter).toObject()["status"].toString(), (*iter).toObject()["fee"].toDouble());
+            Order order;
+            order.currency_pair = (*iter).toObject()["currency_pair"].toString();
+            order.side = (*iter).toObject()["side"].toString();
+            order.avg_price = (*iter).toObject()["avg_price"].toInt();
+            order.price = (*iter).toObject()["price"].toInt();
+            order.order_amount = (*iter).toObject()["order_amount"].toDouble();
+            order.filled_amount = (*iter).toObject()["filled_amount"].toDouble();
+            order.order_total = (*iter).toObject()["order_total"].toInt();
+            order.filled_total = (*iter).toObject()["filled_total"].toInt();
+            order.created_at = (*iter).toObject()["created_at"].toInt();
+            order.last_filled_at = (*iter).toObject()["last_filled_at"].toInt();
+            order.status = (*iter).toObject()["status"].toString();
+            order.fee = (*iter).toObject()["fee"].toDouble();
 
             orders->insert((*iter).toObject()["id"].toInt(), order);
         }
         reply->close();
     });
-*/
+
     return true;
 }
